031224d4e585ed1136b3cbf977a3f3dac96c2fb8
Dennis <dmiller1623@gmail.com>
2019-01-15 13:50:12 -0700

    Add get and listen

--- server.js
+++ server.js
@@ -3,3 +3,11 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Pet Box';
+
+app.get('/', (request, response) => {
+  response.send('Oh hey Pet Box');
+});
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}.`);
+});
