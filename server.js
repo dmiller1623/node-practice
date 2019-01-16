3164e8b036c4e71c74d666ca8577421d1b733b5b
Dennis <dmiller1623@gmail.com>
2019-01-16 14:39:44 -0700

    Add post method for adding someone

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 app.locals.people = [
-  {name: 'dennis', age: 28},
-  {name: 'zack', age: 17},
-  {name: 'selma', age: 49},
-  {name: 'mikaela', age: 19}
+  {name: 'dennis', age: 28, id: 1},
+  {name: 'zack', age: 17, id: 2},
+  {name: 'selma', age: 49, id: 3},
+  {name: 'mikaela', age: 19, id: 4}
 ]
 
 app.get('/', (request, response) => {
@@ -14,6 +14,17 @@ app.get('/', (request, response) => {
 
   return response.json(family)
 });
+
+app.post('/api/v1/fam', (request, response) => {
+  const newMember = response.body;
+  console.log(newMember)
+  // const id = app.locals.people.length + 1
+
+  // if(newMember) {
+  //   app.locals.people.push({ newMember, id })
+  //   return response.status(201).json({ id, newMember })
+  // }
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
