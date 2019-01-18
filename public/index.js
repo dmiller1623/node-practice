d485754d3eeb8198372b4ba2ef7dfe6703711290
Dennis <dmiller1623@gmail.com>
2019-01-18 12:27:41 -0700

    Add post method on frontend to post new members to the database

--- public/index.js
+++ public/index.js
@@ -4,16 +4,24 @@ const nameInput = document.querySelector('.name-input')
 
 const addMember = async (event) => {
   event.preventDefault()
+  let newMember = {
+    name: nameInput.value,
+    age: parseInt(ageInput.value)
+  }
 
-
-  const response = await fetch('/api/v1/fam', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify()
-  })
-  
+  console.log(newMember)
+  try {
+    const response = await fetch('/api/v1/fam', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(newMember)
+    })
+    return await response.json()
+  } catch (error) {
+    throw new Error(error.message)
+  }
 }
 
 
--- server.js
+++ server.js
@@ -11,12 +11,12 @@ app.use( bodyParser.json() );
 app.use(express.static('public'))
 
 app.set('port', process.env.PORT || 3000);
-app.locals.people = [
-  {name: 'dennis', age: 28, id: 1},
-  {name: 'zack', age: 17, id: 2},
-  {name: 'selma', age: 49, id: 3},
-  {name: 'mikaela', age: 19, id: 4}
-]
+// app.locals.people = [
+//   {name: 'dennis', age: 28, id: 1},
+//   {name: 'zack', age: 17, id: 2},
+//   {name: 'selma', age: 49, id: 3},
+//   {name: 'mikaela', age: 19, id: 4}
+// ]
 
 app.get('/api/v1/fam', (request, response) => {
   const family = app.locals.people
@@ -33,22 +33,22 @@ app.get('/api/v1/fam/:id', (request, response) => {
   return response.json(foundMember)
 })
 
-app.post('/api/v1/fam/', (request, response) => {
-  const fam = app.locals.people;
-  const id = app.locals.people.length + 1
-  const newMember = request.body
+// app.post('/api/v1/fam/', (request, response) => {
+//   const fam = app.locals.people;
+//   const id = app.locals.people.length + 1
+//   const newMember = request.body
 
-  fam.push({ newMember, id })
+//   fam.push({ newMember, id })
 
-  return response.json(fam)
-})
+//   return response.json(fam)
+// })
 
 app.post('/api/v1/fam', (request, response) => {
   const newMember = request.body;
 
   database('members').insert(newMember, 'id')
-    .then(paper => {
-      response.status(201).json({ id: newMember[0] })
+    .then(member => {
+      response.status(201).json({ id: member[0] })
     })
 })
 
