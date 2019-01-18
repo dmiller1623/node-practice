eb6852c6fdd352f3d6c8d814942ce536e8bcd0ad
Dennis <dmiller1623@gmail.com>
2019-01-18 11:58:03 -0700

    Add configuration for development and connection to the backend

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 const bodyParser = require('body-parser');
 
 app.use( bodyParser.json() );
