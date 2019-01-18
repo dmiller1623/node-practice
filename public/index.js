e3901a12d9330b1359fac2c44b450fdb88d1008b
Dennis <dmiller1623@gmail.com>
2019-01-18 12:10:34 -0700

    Add name and age selectors

--- public/index.js
+++ public/index.js
@@ -1,7 +1,18 @@
 const addMemberButton = document.querySelector('.add-member')
+const ageInput = document.querySelector('.age-input')
+const nameInput = document.querySelector('.name-input')
 
-const addMember = (event) => {
+const addMember = async (event) => {
   event.preventDefault()
+
+
+  const response = await fetch('/api/v1/fam', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify()
+  })
   
 }
 
