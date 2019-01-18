61bdaf2da032ec0092afcee76f0862752b9d9efc
Dennis <dmiller1623@gmail.com>
2019-01-18 11:48:57 -0700

    Add initial migration function for adding a member

--- db/migrations/20190117120533_intital.js
+++ db/migrations/20190117120533_intital.js
@@ -1,6 +1,14 @@
 
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('members', function(table) {
+      table.increments('id').primary();
+      table.string('name');
+      table.interger('age');
+
+      table.timestamps(true, true);
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
