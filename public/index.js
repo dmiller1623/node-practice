265464d6dbc4c9929e26fefc9c1bcf2bae5a2711
Dennis <dmiller1623@gmail.com>
2019-01-23 20:20:29 -0700

    Add display member function

--- public/index.js
+++ public/index.js
@@ -1,6 +1,7 @@
 const addMemberButton = document.querySelector('.add-member')
 const ageInput = document.querySelector('.age-input')
 const nameInput = document.querySelector('.name-input')
+const familySection = document.querySelector('.family')
 
 let family = []
 
@@ -9,7 +10,19 @@ const getAllMembers = async () => {
   const response = await fetch('/api/v1/fam');
   const familyMembers = await response.json();
   family = familyMembers
-  console.log(family)
+  displayMembers();
+}
+
+const displayMembers = () => {
+  family.forEach((member, index) => {
+    console.log(member)
+    familySection.append(`
+      <div>
+        <h1>${member.name}</h1>
+        <h1>${member.age}</h1>
+      </div>
+    `)
+  })
 }
 
 const addMember = async (event) => {
@@ -18,8 +31,6 @@ const addMember = async (event) => {
     name: nameInput.value,
     age: parseInt(ageInput.value)
   }
-
-  console.log(newMember)
   try {
     const response = await fetch('/api/v1/fam', {
       method: 'POST',
